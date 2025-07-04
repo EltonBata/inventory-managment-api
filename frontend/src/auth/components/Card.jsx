7365aab303c45de26c658aeb895bc6cb66e78239
EltonBata <eltonbata14@gmail.com>
2025-07-04 20:49:49 +0200

    feat(login): put some styling to login page
    - install some packages

--- frontend/src/auth/components/Card.jsx
+++ frontend/src/auth/components/Card.jsx
@@ -1,9 +1,9 @@
-const Card = ({ children, wd = 'w-2/5' }) => {
+const Card = ({ children, wd = 'w-full sm:w-2/3 md:w-3/5 lg:w-2/5' }) => {
   return (
-    <div className="w-dvw h-dvh flex items-center justify-center border">
-      <div className={`card ${wd} bg-white card-md shadow-sm rounded-box`}>
+    <div className="w-dvw h-dvh flex items-center justify-center bg-linear-to-r from-slate-200 to-slate-500 p-3">
+      <div className={`card ${wd} glass card-md shadow-md rounded-box border`}>
         <div className="card-body px-6">
-          <h2 className="card-title tracking-wid flex justify-center py-4">
+          <h2 className="card-title tracking-wid flex justify-center py-4 text-xl">
             <span className="text-sky-800">INVENTORY</span>{" "}
             <span className="text-slate-600">MANAGMENT</span>
           </h2>
