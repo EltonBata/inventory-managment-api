10b117d8f2756ef9ea37384a7bedff9ae3fa8ee0
EltonBata <eltonbata14@gmail.com>
2025-07-07 13:11:45 +0200

    feat(404): add not-found-page

--- frontend/src/routes/Routes.jsx
+++ frontend/src/routes/Routes.jsx
@@ -2,6 +2,8 @@ import { Routes, Route, Navigate } from "react-router-dom";
 import AuthLayout from "../pages/auth/AuthLayout.jsx";
 import LoginPage from "../pages/auth/LoginPage.jsx";
 import RegisterPage from "../pages/auth/RegisterPage.jsx";
+import ErrorsLayout from "../pages/errors/ErrorsLayout.jsx";
+import NotFoundPage from "../pages/errors/NotFoundPage.jsx";
 
 const RoutesList = () => {
   return (
@@ -10,6 +12,10 @@ const RoutesList = () => {
         <Route index element={<LoginPage />}></Route>
         <Route path="/login" element={<Navigate to="/" />}></Route>
         <Route path="/register" element={<RegisterPage />} />
+      </Route>
+
+      <Route path="/" element={<ErrorsLayout />}>
+        <Route path="*" element={<NotFoundPage />}></Route>
       </Route>
     </Routes>
   );
